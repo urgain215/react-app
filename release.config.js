8e4886608d9de854314d00dd26627b6e0091d78e
Urgain Nurboo <urgainnubu439@gmail.com>
2023-07-04 11:12:40 +0530

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+modules.export = {
+    branches: "master",
+    repositoryUrl: "https://github.com/urgain215/react-app/",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+};
