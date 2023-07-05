9ae7ee9bf2932d7c9d1eafdfbbf2eff299863a29
Urgain Nurboo <urgainnubu439@gmail.com>
2023-07-05 12:39:58 +0530

    fix: bug fix

--- release.config.js
+++ release.config.js
@@ -6,8 +6,8 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github",{
             assets:[
-                {path: build.zip, label: build},
-                {path: coverage.zip, label: coverage}
+                {path: "build.zip", label: "build"},
+                {path: "coverage.zip", label: "coverage"}
             ]
         }]
     ]
