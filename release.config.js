d7fbe8b99f08ae2b008b489d165b9c482355b2f6
Urgain Nurboo <urgainnubu439@gmail.com>
2023-07-05 14:38:32 +0530

    fix: Reconfigured

--- release.config.js
+++ release.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/urgain215/react-app/",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        ["@semantic-release/github",{
-            assets:[
-                {path: "build.zip", label: "build"},
-                {path: "coverage.zip", label: "coverage"}
-            ]
-        }]
-    ]
+  branches: "master",
+  repositoryUrl: "https://github.com/urgain215/react-app/",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "build" },
+          { path: "coverage.zip", label: "coverage" },
+        ],
+      },
+    ],
+  ],
 };
